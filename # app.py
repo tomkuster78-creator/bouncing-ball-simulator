69fc1fad354cdace2082aa3b2e2576075846731a
tomkuster78-creator <tomkuster78@gmail.com>
2026-03-18 18:45:44 +0100

    Update # app.py

--- # app.py
+++ # app.py
@@ -15,8 +15,8 @@ ball_type = st.selectbox(
 )
 
 overpressure = st.slider("Overpressure (bar)", 0.1, 5.0, 0.5)
-bounces = st.slider("Number of bounces", 1, 10, 3)
 H = st.slider("Drop height (m)", 0.5, 5.0, 1.0)
+bounces = st.slider("Number of bounces", 1, 10, 3)
 
 
 # ------------------ BALL SETTINGS ------------------
